a3cce6c127729d21e93832592362b508d454967f
Jgarcia3010 <jgarcia@xetechs.group>
2025-12-02 15:35:28 -0600

    Update __manifest__.py

--- libros_conta/__manifest__.py
+++ libros_conta/__manifest__.py
@@ -8,26 +8,28 @@
     'description': 'Módulo para reportes de libros de compras y ventas',
     'website': "http://www.guilles.website",
     
-   
-    'depends': ['base', 'account', 'purchase', 'product', 'odoo-payment-journal'], 
+    # 1. CORRECCIÓN: Quitamos 'odoo-payment-journal' (causa error si no existe)
+    'depends': ['base', 'account', 'purchase', 'product'], 
 
     'data': [
         'security/ir.model.access.csv',
+        'reports/reports_view.xml',       
         'views/libros_templates.xml',
         'data/libros.xml',
         'wizards/extractos_de_cuentas.xml',
-        'wizards/pagos.xml',
-        'views/forms.xml',
+        'wizards/pagos.xml',               
         'views/products.xml',
         'views/partner.xml',
         'views/account_views.xml',
         'views/account_journal_views.xml',
-        'views/account_payment_views.xml'
+        'views/account_payment_views.xml',
+        'views/bank_account.xml',          # Este es el correcto para bancos
     ],
 
     'assets': {
         'web.assets_backend': [
             'libros_conta/static/libros.xml',
+            'libros_conta/static/libros.js', 
         ],
     },
 
